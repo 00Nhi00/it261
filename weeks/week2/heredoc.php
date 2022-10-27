aa3c23043103381dffa659a2e894695f8d7e2620
Nhi <91705578+00Nhi00@users.noreply.github.com>
2022-10-26 23:46:56 -0700

    adding heredoc.php

---
+++ weeks/week2/heredoc.php
@@ -0,0 +1,8 @@
+<?php
+$str = <<<EOD
+One of my favorite books is the Handmaid's Tale, written by Margaret Atwood, and is presently a miniseries on HULU. Hulu's viewing<br>
+audience is extremely excited about the miniseries and looks forward to the 5th season of the award-winning "Handmaid's Tale!"<br><br>
+
+Elizabeth Moss' rendition as June is right on! I liked the show's first 2 seasons!
+EOD;
+echo $str;
