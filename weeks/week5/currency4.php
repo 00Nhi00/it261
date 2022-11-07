2608c531e981172d227e2eedff90986715d363a5
Nhi <thaonhi.nguyen411@gmail.com>
2022-11-07 02:06:09 -0800

    week 5 exercises & extra credit

---
+++ weeks/week5/currency4.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Currency 4 extra credit</title>
+  <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<!-- https://www.geeksforgeeks.org/how-to-prevent-xss-with-html-php/ -->
+<form action="<?php echo htmlspecialchars ($_SERVER['PHP_SELF']) ;?>" method="post">
+<fieldset>
+  <label>NAME</label>
+  <input type="text" name="name" value="<?php if(isset($_POST['name'])) echo htmlspecialchars($_POST['name']);?>">
+
+  <label>EMAIL</label>
+  <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
+
+  <label>How much money do you have?</label>
+
+  <input type="number" name="amount" value="<?php if(isset($_POST['amount'])) echo htmlspecialchars($_POST['amount']);?>">
+  <!-- time for our radio button that has an additional attribute of VALUE -->
+  <label>Choose your currency</label>
+  <ul>
+    <li><input type="radio" name="currency" value="0.017" <?php if(isset($_POST['currency']) && $_POST['currency'] == 0.017) echo 'checked="checked" ' ;?> > Rubles </li>
+    <li><input type="radio" name="currency" value="0.76" <?php if(isset($_POST['currency']) && $_POST['currency'] == 0.76) echo 'checked="checked" ' ;?> > Canadian Dollars </li>
+    <li><input type="radio" name="currency" value="1.15" <?php if(isset($_POST['currency']) && $_POST['currency'] == 1.15) echo 'checked="checked" ' ;?> > Pounds </li>
+    <li><input type="radio" name="currency" value="1.00" <?php if(isset($_POST['currency']) && $_POST['currency'] == 1.00) echo 'checked="checked" ' ;?> > Euros </li>
+    <li><input type="radio" name="currency" value="0.0074" <?php if(isset($_POST['currency']) && $_POST['currency'] == 0.0074) echo 'checked="checked" ' ;?> > Yen </li>
+  
+</ul>
+<label>Chooose your banking institution</label>
+<select name="bank">
+  <option value="" NULL <?php if(isset($_POST['bank']) && $_POST['bank']==NULL) echo 'selected="unselected"';?>>Select one!</option>
+  <option value="boa" <?php if(isset($_POST['bank']) && $_POST['bank']== 'boa') echo 'selected="unselected"';?>>Bank of America</option>
+  <option value="chase" <?php if(isset($_POST['bank']) && $_POST['bank']=='chase') echo 'selected="unselected"';?>>Chase Bank</option>
+  <option value="banner" <?php if(isset($_POST['bank']) && $_POST['bank']=='banner') echo 'selected="unselected"';?>>Banner Bank</option>
+  <option value="wells" <?php if(isset($_POST['bank']) && $_POST['bank']=='wells') echo 'selected="unselected"';?>>Wells Fargo</option>
+  <option value="becu" <?php if(isset($_POST['bank']) && $_POST['bank']=='becu') echo 'selected="unselected"';?>>Boeing Credit Union</option>
+
+</select>
+<input type="submit" value="Convert it">
+
+  <p><a href="">Reset it!</a></p>
+</fieldset>
+</form>
+  
+<?php
+//we'll start with the server request method
+// then we will ask ourselves if any fields are empty
+// if the fields are not empty, we will elseif they are set
+
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+  if(empty($_POST['name'])){
+    echo '<p class="error">Please fill out your name!</p>';
+  }
+  if(empty($_POST['email'])){
+    echo '<p class="error">Please fill out your email!</p>';
+  }
+  if(empty($_POST['amount'])){
+    echo '<p class="error">Please fill out the amount!</p>';
+  }
+  if(empty($_POST['currency'])){
+    echo '<p class="error">Please check your currency!</p>';
+  }
+  if($_POST['bank']==NULL){
+    echo '<p class="error">Please choose your banking institution!</p>';
+  }
+  // if(empty($_POST['bank'])){
+  //   echo'<p class="error">Please fill out your bank</p>';
+  // }
+
+ 
+ 
+  if(isset($_POST['name'],
+  $_POST['email'],
+  $_POST['amount'],
+  $_POST['currency'], 
+  $_POST['bank'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $amount = floatval($_POST['amount']);
+  $currency = floatval($_POST['currency']);
+  $bank = $_POST['bank'];
+  $dollars = $amount * $currency;
+
+  if(!empty($name && $email && $amount && $currency && $bank)) {
+
+  echo '
+  <div class="box">
+  <h2><b>Hello '.$name.',</b></h2>
+  <p>You now have <b>'.number_format($dollars, 2).' American dollars </b> and we will be emailing you at '.$email.' with your information, as well as depositing your funds at <b>'.$bank.' bank!</b></p>
+  </div>
+  ';
+
+
+if ($dollars >= 1000){
+  echo '
+  <div class="box2">
+  <p><b>I am REALLY happy, because I have $'.number_format($dollars, 2).' American dollars!</b></p>
+  <iframe width="420" height="315" src="https://www.youtube.com/embed/ufbOHl1mmYk"></iframe>
+  </div>';
+} else {
+  echo'
+  <div class="box2">
+  <p><b>I am NOT happy, because I have $'.number_format($dollars, 2).' American dollars!</b></p>
+  <iframe width="420" height="315" src="https://www.youtube.com/embed/YNwS2o8VeUY"></iframe>
+  </div>';
+}
+} 
+}
+}
+?>
+</body>
+</html>
