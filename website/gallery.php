ce5f396bfbe6063178908d6dfd522714e533c693
Nhi <91705578+00Nhi00@users.noreply.github.com>
2022-11-25 13:54:06 -0800

    adding photos

--- website/gallery.php
+++ website/gallery.php
@@ -1,9 +1,9 @@
 <?php
-$singer['Taylor_Swift'] = 'Taylor was born on December 13, 1989';
-$singer['Justin_Bieber'] = 'Justin was born on March 1, 1994';
-$singer ['Ariana_Grande'] = 'Ariana was born on June 26, 1993';
-$singer['Ed_Sheeran'] = 'EdShee was born on February 17, 1991';
-$singer ['Adele_Adkins'] = 'AdeleA was born on May 5, 1988';
+$singer['Taylor_Swift'] = 'Taylor was born on December 13, 1989 - Singer';
+$singer['Justin_Bieber'] = 'Justin was born on March 1, 1994 - Singer';
+$singer ['Ariana_Grande'] = 'Ariana was born on June 26, 1993 - Singer';
+$singer['Ed_Sheeran'] = 'EdShee was born on February 17, 1991 - Singer';
+$singer ['Adele_Adkins'] = 'AdeleA was born on May 5, 1988 - Singer';
 ?>
 
 <!DOCTYPE html>
@@ -37,7 +37,7 @@ $singer ['Adele_Adkins'] = 'AdeleA was born on May 5, 1988';
         <tr>
             <td><img src="./images/<?php echo substr($image, 0, 6);?>.png" alt="<?php echo $name;?>"></td>
             <td><?php echo str_replace('_',' ', $name);?></td>
-            <td><?php echo substr($image, 19, 18);?></td>
+            <td><?php echo substr($image, 19, 27);?></td>
             <td><img src="images/<?php echo $name;?>.png" alt="<?php echo $name;?>"></td>
         </tr>
         <?php endforeach ;?>
