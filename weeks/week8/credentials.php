58f766b724d2e5c914316e5d3ef657327961e07a
Nhi <thaonhi.nguyen411@gmail.com>
2022-11-29 17:08:40 -0800

    week 8 cont.

---
+++ weeks/week8/credentials.php
@@ -0,0 +1,12 @@
+<?php
+// nhithith_school_projects -- name of my database
+// nhithith_school_projects_user -- name of my user 
+
+
+
+define('DB_NAME', 'nhithith_school_projects');
+define('DB_USER', 'nhithith_school_projects_user');
+define('DB_PASSWORD', 'NhiNguyen');
+define('DB_HOST', 'localhost');
+
+?>
